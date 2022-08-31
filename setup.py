4d5e3ee4007fdeebc7f252b912742c8aa05dc3b1
Ross McFarland <rwmcfa1@gmail.com>
2022-08-31 06:15:14 -0700

    scripts to rule them all, full setup.py/requirements, pytest, hooks

--- setup.py
+++ setup.py
@@ -20,17 +20,28 @@ packages = [
 
 requires = [
     'requests>=1.2.0',
-    'futures>=2.1.3; python_version<"3.2"'
 ]
+
+tests_require = ('pytest>=6.2.5', 'pytest-cov>=3.0.0', 'pytest-network>=0.0.1')
 
 setup(
     name='requests-futures',
     version=requests_futures.__version__,
     description='Asynchronous Python HTTP for Humans.',
+    extras_require={
+        'dev': tests_require
+        + (
+            'black>=22.3.0',
+            'build>=0.7.0',
+            'pyflakes>=2.2.0',
+            'readme_renderer[rst]>=26.0',
+            'twine>=3.4.2',
+        )
+    },
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
     author='Ross McFarland',
-    author_email='rwmcfa1@neces.com',
+    author_email='rwmcfa1@rwmcfa1.com',
     packages=packages,
     package_dir={'requests_futures': 'requests_futures'},
     package_data={'requests_futures': ['LICENSE', 'README.rst']},
@@ -38,6 +49,7 @@ setup(
     install_requires=requires,
     setup_requires=['setuptools>=38.6.1'],
     license='Apache License v2',
+    tests_require=tests_require,
     url='https://github.com/ross/requests-futures',
     zip_safe=False,
     classifiers=[
