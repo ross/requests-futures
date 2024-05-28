92a3359d0c2bac012cf3232fc696b1817373fbd4
Ross McFarland <rwmcfa1@gmail.com>
2024-05-28 11:04:03 -0700

    pin greenlet to 2.0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ packages = ['requests_futures']
 requires = ['requests>=1.2.0']
 
 tests_require = (
-    'greenlet<=3.0',
+    'greenlet==2.0.2',
     'pytest>=6.2.5',
     'pytest-cov>=3.0.0',
     'pytest-httpbin>=2.0.0',
