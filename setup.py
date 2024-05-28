ff5036e5532bb4949c97b05eb7f069a90a114e1b
Ross McFarland <rwmcfa1@gmail.com>
2024-05-28 10:33:29 -0700

    clamp docutils until 3.8 is gone

--- setup.py
+++ setup.py
@@ -35,6 +35,9 @@ setup(
         + (
             'black>=24.3.0,<25.0.0',
             'build>=0.7.0',
+            # docutils 0.21.x bumped to >=3.9 and 3.8 is still active. we'll
+            # have to clamp it down until we remove 3.8
+            'docutils<=0.20.1',
             'isort>=5.11.4',
             'pyflakes>=2.2.0',
             'readme_renderer[rst]>=26.0',
